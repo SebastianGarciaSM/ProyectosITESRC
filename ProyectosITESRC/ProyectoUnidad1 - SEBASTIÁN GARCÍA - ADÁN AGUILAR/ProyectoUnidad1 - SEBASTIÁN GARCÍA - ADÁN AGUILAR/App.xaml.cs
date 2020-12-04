96354045004e1c8bc4cf1741a5eb4b13ee7c1155
Sebastián García <171G0228@rcarbonifera.tecnmx.mx>
2020-12-03 19:01:42 -0600

    added

---
+++ ProyectosITESRC/ProyectoUnidad1 - SEBASTIÁN GARCÍA - ADÁN AGUILAR/ProyectoUnidad1 - SEBASTIÁN GARCÍA - ADÁN AGUILAR/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProyectoUnidad1___SEBASTIÁN_GARCÍA___ADÁN_AGUILAR
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ProyectosITESRC/ProyectoUnidad1 - SEBASTIÁN GARCÍA - ADÁN AGUILAR/ProyectoUnidad1 - SEBASTIÁN GARCÍA - ADÁN AGUILAR/vntAgregarEmpleado.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoUnidad1___SEBASTIÁN_GARCÍA___ADÁN_AGUILAR
+{
+    /// <summary>
+    /// Lógica de interacción para vntAgregarEmpleado.xaml
+    /// </summary>
+    public partial class vntAgregarEmpleado : Window
+    {
+        public vntAgregarEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
